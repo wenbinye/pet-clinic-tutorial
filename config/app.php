77d3cbbc619f0d8f19af9629000440c7746896a1
Ye Wenbin <yewenbin@winwin-inc.com>
2018-07-02 17:22:33 +0800

    add twig template

--- config/app.php
+++ config/app.php
@@ -3,7 +3,11 @@
 return [
     'providers' => [
         kuiper\boot\providers\WebApplicationProvider::class,
+        kuiper\boot\providers\TwigViewProvider::class,
     ],
     'base_path' => realpath(__DIR__.'/..'),
     'runtime_path' => '{app.base_path}/runtime',
+    'views_path' => '{app.base_path}/resources/views',
+    'static_base_uri' => 'http://cdn.17gaoda.com/winwin/0.1.0/pet-clinic',
+    'dev_mode' => true,
 ];
--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -2,10 +2,20 @@
 
 namespace winwin\petClinic\controllers;
 
+use kuiper\di\annotation\Inject;
+use kuiper\web\ViewInterface;
+
 class IndexController extends \kuiper\web\Controller
 {
+    /**
+     * @Inject
+     *
+     * @var ViewInterface
+     */
+    private $view;
+
     public function index()
     {
-        $this->response->getBody()->write('It works!');
+        $this->response->getBody()->write($this->view->render('index/welcome.html'));
     }
 }
